47bf0dbeca54b771de7bbab65e862d88e48ca7d4
Dakov <inj.dakov@gmail.com>
2017-12-02 14:04:12 +0200

    Used Windows Hooks for catching global events.

--- MouseEventCounter/FormMain.cs
+++ MouseEventCounter/FormMain.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MouseEventCounter
@@ -21,13 +14,14 @@ namespace MouseEventCounter
         {
             InitializeComponent();
 
-            //MouseWheel += Form_MouseWheel;
-            //MouseClick += Form1_MouseClick;
-            //MouseDoubleClick += Form1_MouseDoubleClick;
+            MouseHook.Start();
 
-            MouseWheel += FormMain_MouseWheel;
-            MouseClick += FormMain_MouseClick;
-            MouseDoubleClick += FormMain_MouseDoubleClick;
+            MouseHook.MouseActionLeftCick += new EventHandler(EventLeftCick);
+            MouseHook.MouseActionMiddleCick += new EventHandler(EventMiddleCick);
+            MouseHook.MouseActionRightCick += new EventHandler(EventRightCick);
+            MouseHook.MouseActionWheel += new EventHandler(EventWheel);
+
+            Closed += FormMain_Closed;
         }
 
         private void FormMain_Load(object sender, EventArgs e)
@@ -38,46 +32,37 @@ namespace MouseEventCounter
             txtScrollClicks.Text = _totalMiddleClicks.ToString();
         }
 
-        private void FormMain_MouseWheel(object sender, MouseEventArgs e)
+        private void FormMain_Closed(object sender, EventArgs e)
         {
-            // This guarantee every positive value with step 3. It's a magic number.
-            _totalDelta = _totalDelta + (e.Delta >= 0 ? e.Delta : 0 - e.Delta) / 40;
+            MouseHook.Start();
+        }
+
+        private void EventLeftCick(object sender, EventArgs e)
+        {
+            _totalLeftClicks += 1;
+
+            txtLeftClicks.Text = _totalLeftClicks.ToString();
+        }
+
+        private void EventMiddleCick(object sender, EventArgs e)
+        {
+            _totalMiddleClicks += 1;
+
+            txtScrollClicks.Text = _totalMiddleClicks.ToString();
+        }
+
+        private void EventRightCick(object sender, EventArgs e)
+        {
+            _totalRightClicks += 1;
+
+            txtRightClicks.Text = _totalRightClicks.ToString();
+        }
+
+        private void EventWheel(object sender, EventArgs e)
+        {
+            _totalDelta += 3; // Magic number, 3 millimeter movement. :)
 
             txtDistance.Text = _totalDelta.ToString();
-        }
-
-        private void FormMain_MouseClick(object sender, MouseEventArgs e)
-        {
-            ClickCounter(e);
-        }
-
-        private void FormMain_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            ClickCounter(e);
-        }
-
-        private void ClickCounter(MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                _totalLeftClicks += 1;
-
-                txtLeftClicks.Text = _totalLeftClicks.ToString();
-            }
-
-            if (e.Button == MouseButtons.Right)
-            {
-                _totalRightClicks += 1;
-
-                txtRightClicks.Text = _totalRightClicks.ToString();
-            }
-
-            if (e.Button == MouseButtons.Middle)
-            {
-                _totalMiddleClicks += 1;
-
-                txtScrollClicks.Text = _totalMiddleClicks.ToString();
-            }
         }
     }
 }
---
+++ MouseEventCounter/MouseHook.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MouseEventCounter
+{
+    public static class MouseHook
+    {
+        public static event EventHandler MouseActionLeftCick = delegate { };
+
+        public static event EventHandler MouseActionMiddleCick = delegate { };
+
+        public static event EventHandler MouseActionRightCick = delegate { };
+
+        public static event EventHandler MouseActionWheel = delegate { };
+
+        public static void Start()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public static void Stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MouseActionLeftCick(null, new EventArgs());
+            }
+
+            if (nCode >= 0 && MouseMessages.WM_MBUTTONDOWNP == (MouseMessages)wParam)
+            {
+                MouseActionMiddleCick(null, new EventArgs());
+            }
+
+            if (nCode >= 0 && MouseMessages.WM_RBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MouseActionRightCick(null, new EventArgs());
+            }
+
+            if (nCode >= 0 && MouseMessages.WM_MOUSEWHEEL == (MouseMessages)wParam)
+            {
+                MouseActionWheel(null, new EventArgs());
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205,
+
+            WM_MBUTTONDOWNP = 0x0207
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
